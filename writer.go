b62d7883185788b1ca595d512d93b0fe5a607ca2
klauspost <klauspost@gmail.com>
2015-10-14 15:35:24 +0200

    Make first runnable test pass.

---
+++ writer.go
@@ -0,0 +1,187 @@
+package dedup
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
+	"errors"
+	"io"
+	"sync"
+)
+
+type writer struct {
+	blks    io.Writer
+	idx     io.Writer
+	size    int
+	index   map[[sha1.Size]byte]struct{}
+	input   chan block
+	exited  chan struct{}
+	cur     []byte
+	off     int
+	buffers sync.Pool
+	vari64  []byte
+}
+
+type block struct {
+	data     []byte
+	sha1hash [sha1.Size]byte
+}
+
+var ErrSizeTooSmall = errors.New("block size too small")
+
+func NewWriter(index io.Writer, blocks io.Writer, size uint) (io.WriteCloser, error) {
+	r := &writer{
+		blks:   blocks,
+		idx:    index,
+		size:   int(size),
+		index:  make(map[[sha1.Size]byte]struct{}),
+		input:  make(chan block, 10),
+		exited: make(chan struct{}, 0),
+		cur:    make([]byte, size),
+		vari64: make([]byte, binary.MaxVarintLen64),
+	}
+	r.buffers.New = func() interface{} { return make([]byte, size) }
+
+	if r.size <= sha1.Size {
+		return nil, ErrSizeTooSmall
+	}
+
+	r.putUint64(1) // Format
+	r.putUint64(uint64(size))
+	go r.hasher()
+	return r, nil
+}
+
+// putUint64 will Write uint64 value to index stream.
+func (r *writer) putUint64(v uint64) error {
+	n := binary.PutUvarint(r.vari64, v)
+	n2, err := r.idx.Write(r.vari64[:n])
+	if err != nil {
+		return err
+	}
+	if n2 != n {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+func (r *writer) Write(b []byte) (n int, err error) {
+	written := 0
+	for len(b) > 0 {
+		n := copy(r.cur[r.off:], b)
+		b = b[n:]
+		r.off += n
+		written += n
+		if r.off == r.size {
+			r.input <- block{data: r.cur}
+			r.cur = r.buffers.Get().([]byte)
+			r.off = 0
+		}
+	}
+	return written, nil
+}
+
+var emptyHash = [sha1.Size]byte{}
+
+func (r *writer) Close() (err error) {
+	select {
+	case <-r.exited:
+		return nil
+	default:
+	}
+	close(r.input)
+	<-r.exited
+
+	// Insert empty hash into index to indicate EOF
+	buf := bytes.NewBuffer(emptyHash[:])
+	n, err := io.Copy(r.idx, buf)
+	if err != nil {
+		return err
+	}
+	if n != sha1.Size {
+		return errors.New("close short copy")
+	}
+	// Insert length of remaining data into index
+	r.putUint64(uint64(r.off))
+
+	buf = bytes.NewBuffer(r.cur[0:r.off])
+	n, err = io.Copy(r.blks, buf)
+	if err != nil {
+		return err
+	}
+	if int(n) != r.off {
+		return errors.New("r.cur short copy")
+	}
+
+	return nil
+}
+
+func (r *writer) hasher() {
+	hasher := sha1.New()
+	defer close(r.exited)
+	for b := range r.input {
+		buf := bytes.NewBuffer(b.data)
+		hasher.Reset()
+		n, err := io.Copy(hasher, buf)
+		if err != nil {
+			panic(err.Error())
+		}
+		h := hasher.Sum(b.sha1hash[:0])
+		_, ok := r.index[b.sha1hash]
+		if !ok {
+
+			r.index[b.sha1hash] = struct{}{}
+			buf = bytes.NewBuffer(b.data)
+			n, err = io.Copy(r.blks, buf)
+			if err != nil {
+				panic(err.Error())
+			}
+			if int(n) != len(b.data) {
+				panic("short write")
+			}
+		}
+		n2, err := r.idx.Write(h)
+		if err != nil {
+			panic(err.Error())
+		}
+		if n2 != sha1.Size {
+			panic("short write")
+		}
+		r.buffers.Put(b.data)
+	}
+}
+
+/*
+ZPAQ: (public domain)
+
+  // Set block and fragment sizes
+  // -fragment N     Set average dedupe fragment size = 2^N KiB (default: 6)
+  if (fragment<0) fragment=0;
+
+  const unsigned blocksize=(1u<<(20+atoi(method.c_str()+1)))-4096;
+  const unsigned MAX_FRAGMENT=fragment>19 || (8128u<<fragment)>blocksize-12
+      ? blocksize-12 : 8128u<<fragment;
+  const unsigned MIN_FRAGMENT=fragment>25 || (64u<<fragment)>MAX_FRAGMENT
+      ? MAX_FRAGMENT : 64u<<fragment;
+
+
+		unsigned char o1[256]={0}; // order 1 context -> predicted byte
+		int c=EOF;  // current byte
+		int hits = 0;  // hits - we can use this to determine
+        while (true) {
+          c=in.get();
+          if (c!=EOF) {
+            if (c==o1[c1]) h=(h+c+1)*314159265u; hits++
+            else h=(h+c+1)*271828182u;
+            o1[c1]=c;
+            c1=c;
+            sha1.put(c);
+            fragbuf[sz++]=c;
+          	if ( sz>=MAX_FRAGMENT || (fragment<=22 && h<(1u<<(22-fragment)) && sz>=MIN_FRAGMENT))
+            	break;
+          } else {
+          	break;
+          }
+        }
+
+*/
---
+++ writer_test.go
@@ -0,0 +1,53 @@
+package dedup_test
+
+import (
+	"testing"
+
+	"bytes"
+	"crypto/rand"
+	"io"
+
+	"github.com/klauspost/dedup"
+)
+
+func TestWriter(t *testing.T) {
+	idx := bytes.Buffer{}
+	data := bytes.Buffer{}
+	input := &bytes.Buffer{}
+
+	const totalinput = 10 << 20
+	_, err := io.CopyN(input, rand.Reader, totalinput)
+	if err != nil {
+		t.Fatal(err)
+	}
+	const size = 64 << 10
+	b := input.Bytes()
+	// Create some duplicates
+	for i := 0; i < 50; i++ {
+		// Read from 10 first blocks
+		src := b[(i%10)*size : (i%10)*size+size]
+		// Write into the following ones
+		dst := b[(10+i)*size : (i+10)*size+size]
+		copy(dst, src)
+	}
+	input = bytes.NewBuffer(b)
+	w, err := dedup.NewWriter(&idx, &data, size)
+	if err != nil {
+		t.Fatal(err)
+	}
+	io.Copy(w, input)
+	err = w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	removed := ((totalinput) - data.Len()) / size
+
+	t.Log("Index size:", idx.Len())
+	t.Log("Data size:", data.Len())
+	// We should get at least 50 blocks
+	if removed < 50 {
+		t.Log()
+	}
+	t.Log("Removed", removed, "blocks")
+
+}
