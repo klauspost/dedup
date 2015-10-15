459a476d5945a601abffb1b7595595169059a026
Klaus Post <klauspost@gmail.com>
2015-10-15 16:55:43 +0200

    Add 4K block test.

--- reader_test.go
+++ reader_test.go
@@ -118,3 +118,60 @@ func BenchmarkReader64K(t *testing.B) {
 		}
 	}
 }
+
+func BenchmarkReader4K(t *testing.B) {
+	idx := &bytes.Buffer{}
+	data := &bytes.Buffer{}
+	input := &bytes.Buffer{}
+
+	const totalinput = 10 << 20
+	_, err := io.CopyN(input, rand.Reader, totalinput)
+	if err != nil {
+		t.Fatal(err)
+	}
+	const size = 4 << 10
+	b := input.Bytes()
+	// Create some duplicates
+	for i := 0; i < 500; i++ {
+		// Read from 10 first blocks
+		src := b[(i%10)*size : (i%10)*size+size]
+		// Write into the following ones
+		dst := b[(10+i)*size : (i+10)*size+size]
+		copy(dst, src)
+	}
+	input = bytes.NewBuffer(b)
+	w, err := dedup.NewFixedWriter(idx, data, size)
+	if err != nil {
+		t.Fatal(err)
+	}
+	io.Copy(w, input)
+	err = w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	index := idx.Bytes()
+	alldata := data.Bytes()
+
+	t.ResetTimer()
+	t.SetBytes(totalinput)
+	for i := 0; i < t.N; i++ {
+		idx = bytes.NewBuffer(index)
+		data = bytes.NewBuffer(alldata)
+		r, err := dedup.NewReader(idx, data)
+		if err != nil {
+			t.Fatal(err)
+		}
+		n, err := io.Copy(ioutil.Discard, r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != int64(len(b)) {
+			t.Fatal("read was short")
+		}
+		err = w.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
